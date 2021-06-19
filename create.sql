abd29f28dccbb1f835b2c86cd512bd6dd269649d
GayathriSundar2 <sundargayathri74@gmail.com>
2021-06-19 23:58:16 +0530

    Corrected the errors

--- create.sql
+++ create.sql
@@ -1,30 +1,30 @@
+DROP TABLE IF EXISTS publisher;
+DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS subjects;
+DROP TABLE IF EXISTS books_subjects;
+
+
 create table publisher  (
-       id serial primary key,
-       name text not null,
-       country text not null
+       id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+       name text,
+       country text
 );
 
 create table books  (
-       id serial primary key,
-       title text not null,
-       publisher integer not null,
+       id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+       title text NOT NULL,
+       publisher INTEGER NOT NULL,
        FOREIGN KEY (publisher) REFERENCES publisher (id)
-       ON UPDATE SET NULL
-       ON DELETE SET NULL
 );
 
 create table subjects  (
-       id serial primary key,
-       name text not null
+       id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+       name text NOT NULL
 );
 
 create table book_subjects  (
-       book integer not null,
-       subject integer not null,
-       FOREIGN KEY (book) REFERENCES books (id)
-       ON UPDATE SET NULL
-       ON DELETE SET NULL,
+       book INTEGER NOT NULL,
+       subject INTEGER NOT NULL,
+       FOREIGN KEY (book) REFERENCES books (id),
        FOREIGN KEY (subject) REFERENCES subjects (id)
-       ON UPDATE SET NULL
-       ON DELETE SET NULL
 );
--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-DELETE FROM books_subject WHERE books_subjects.subject = (SELECT id FROM subjects WHERE name = 'History');
+DELETE FROM books_subjects WHERE books_subjects.subject = (SELECT id FROM subjects WHERE name = 'History');
 DELETE FROM subjects WHERE name='History'; 
