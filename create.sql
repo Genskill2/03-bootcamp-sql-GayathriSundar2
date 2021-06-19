06f54eb39085bcf49621cda3c51f03e6c9b69560
GayathriSundar2 <sundargayathri74@gmail.com>
2021-06-20 00:26:08 +0530

    Errors in create rectified

--- create.sql
+++ create.sql
@@ -4,27 +4,29 @@ DROP TABLE IF EXISTS subjects;
 DROP TABLE IF EXISTS books_subjects;
 
 
-create table publisher  (
-       id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-       name text,
-       country text
+create table publisher(
+     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     name text,
+     country text
 );
 
-create table books  (
-       id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-       title text NOT NULL,
-       publisher INTEGER NOT NULL,
-       FOREIGN KEY (publisher) REFERENCES publisher (id)
+create table books(
+     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     title text NOT NULL,
+     publisher INTEGER NOT NULL,
+     FOREIGN KEY (publisher) REFERENCES publisher(id)
 );
 
-create table subjects  (
-       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-       name text NOT NULL
+create table subjects(
+     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+     name text NOT NULL
 );
 
-create table books_subjects  (
-       book INTEGER NOT NULL,
-       subject INTEGER NOT NULL,
-       FOREIGN KEY (book) REFERENCES books (id),
-       FOREIGN KEY (subject) REFERENCES subjects (id)
+create table books_subjects(
+     book INTEGER NOT NULL,
+     subject INTEGER NOT NULL,
+     FOREIGN KEY (book) REFERENCES books(id),
+     FOREIGN KEY (subject) REFERENCES subjects(id)
 );
+
+
